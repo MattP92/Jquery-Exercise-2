8f1c27474787abca7a9c1da0ab616f6a41a33a0f
MattP92 <39284208+MattP92@users.noreply.github.com>
2018-09-10 18:12:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+const down = $("ul");
 $("button").on("click", () => {
-    const down = $("ul"); 
     down.slideToggle(); 
 })
 
 $(".numbers").on("click", () => {
-    $("section").html("1 2 3 4 5 6 7 8 9 10"); 
+    $("section").html("1 2 3 4 5 6 7 8 9 10");
+    down.slideToggle(); 
 })
 
 $(".letters").on("click", () => {
     $("section").html("A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"); 
+    down.slideToggle(); 
 })
